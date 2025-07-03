302da24ebb9dc7cf8a2e6bf1989b0c817a2e1c2f
Ginanjar Nugroho <anjar.jamil30@gmail.com>
2025-07-04 03:43:51 +0700

    create resource for ecs cluster (yaml, params and deploy)

---
+++ scripts/deploy-ecs-cluster.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STACK_NAME=ecs-cluster-stack
+TEMPLATE_FILE=templates/ecs-cluster.yaml
+PARAMS_FILE=parameters/ecs-cluster-params.json
+REGION=ap-southeast-1
+
+aws cloudformation deploy \
+  --stack-name $STACK_NAME \
+  --template-file $TEMPLATE_FILE \
+  --parameter-overrides file://$PARAMS_FILE \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --region $REGION
